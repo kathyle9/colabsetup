01d8e6d306b7daa4393d1040b4a0960bb928b5fc
kle16 <kle16@jhu.edu>
2019-08-30 10:49:51 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='colabsetup',
+      version='0.1',
+      description='Mounts Google Drive for PyRosetta use in Google Colaboratory',
+      url='https://github.com/kathyle9/colabsetup',
+      author='kathyle9',
+      author_email='kle16@jhu.edu',
+      license='MIT',
+      packages=['colabsetup'],
+      zip_safe=False)
